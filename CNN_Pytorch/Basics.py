2f6b6d39b0ee769f8236267776ae1e5ea3901210
FERBIN12 <fjferbin@gmail.com>
2023-08-08 20:03:33 +0530

    commit

--- CNN_Pytorch/Basics.py
+++ CNN_Pytorch/Basics.py
@@ -1,8 +1,10 @@
+
 import torch
 
 ## used to get dataset and useful image transformations
 import torchvision
 import torchvision.transforms as transforms
+
 
 #Optimization library of pytorch and nn- used as basic building block for our Network graphs
 import torch.optim as optim
@@ -10,5 +12,25 @@ import torch.nn as nn
 
 print("GPU available: {}".format(torch.cuda.is_available()))   ## for printing about the availability of GPU
 
-#CUDA - Nvidia accleration library which is used to enable GPU performance
+# #CUDA - Nvidia accleration library which is used to enable GPU performance
+
+if torch.cuda.is_available():
+  device='cuda'
+else:
+  device='cpu'
+
+transform=transforms.Compose([transforms.ToTensor(),   # nothing but matrices in tensor form to be used by the GPU's
+                              transforms.Normalize((0.5,0),(0.5,0)) ])
+
+# Load our Training Data and specify what transform to use when loading
+trainset = torchvision.datasets.MNIST('mnist', 
+                                      train = True, 
+                                      download = True,
+                                      transform = transform)
+
+# Load our Test Data and specify what transform to use when loading
+testset = torchvision.datasets.MNIST('mnist', 
+                                     train = False,
+                                     download = True,
+                                     transform = transform)
 
