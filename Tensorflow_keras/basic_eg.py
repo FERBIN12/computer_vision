98d86275f1ea30e46b36e2d3c4839724b0d32d51
FERBIN12 <fjferbin@gmail.com>
2023-08-15 20:37:39 +0530

    boooooo

--- Tensorflow_keras/basic_eg.py
+++ Tensorflow_keras/basic_eg.py
@@ -47,3 +47,62 @@ print("The num of classes is :",str(y_test.shape[1]))
 
 num_classes=y_test.shape[1]
 num_pixels=x_train.shape[1]*x_train.shape[2]
+
+## creating the CNN layers for the training
+
+from tensorflow.keras.models import Sequential ## means a model in which each layer is connected to the next 
+from tensorflow.keras.layers import Dense,Dropout,Flatten,Conv2D,MaxPooling2D
+from tensorflow.keras import backend as k
+from tensorflow.keras.optimizers import SGD
+
+ 
+# create a model 
+model=Sequential()
+
+# we create our first convoultional layer with filter size 32 and a layer size of 26*26*32 and for activation we use Relu and set our input image
+
+model.add(Conv2D(32,kernel_size=(3,3),activation='relu',input_shape=input_shape))
+
+# second layer is has 64 filer size of 64 and the size of the layer would be 24,24,64    ## we have gone with the default value of padding and stride
+
+model.add(Conv2D(64,(3,3),activation='relu'))
+
+# next we add the max pool layer 
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+# then we flatten the layer of the CNN 
+model.add(Flatten())
+
+# we connect the flattend to a fully connected layer of size 128  using Dense function
+model.add(Dense(128,activation='relu'))
+
+# Then we finally connect the output to our final layer with an output for each class ie) 10
+
+model.add(Dense(num_classes,activation='softmax'))
+
+# then we compile the whole model using compile function mention the needed loss functions 
+# optimizer and metric ie accuracy and all
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=SGD(0.001),
+              metrics=['accuracy'])
+print(model.summary())
+
+
+# training the model
+
+batch_size=128
+epochs=25
+
+
+history=model.fit(x_train,
+                  y_train,
+                  batch_size=batch_size,
+                  epochs=epochs,
+                  verbose=1,
+                  validation_data=(x_test,y_test))
+
+score=model.evaluate(x_test,y_test,verbose=0)
+print('Test loss',score[0])
+print('Test accuracy',score[1])
+
