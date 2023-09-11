c03c9bf9d2abf6294b3c62e2d61438129c38b151
FERBIN12 <fjferbin@gmail.com>
2023-09-11 15:29:14 +0530

    added_yolonas

--- Yolo/protective_equipment_yolonas.py
+++ Yolo/protective_equipment_yolonas.py
@@ -1,4 +1,158 @@
+from super_gradients.training import Trainer
+from super_gradients.training import dataloaders
+from super_gradients.training.dataloaders.dataloaders import coco_detection_yolo_format_train,coco_detection_yolo_format_val
+from IPython.display import clear_output
+from super_gradients.training.losses import PPYoloELoss
+from super_gradients.training import dataloaders,models
+from super_gradients.training.metrics import DetectionMetrics_050
+from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
 from roboflow import Roboflow
+
+# now we set the checkpoint directory and instantiate the trainer 
+check_point='checkpoints'
+trainer=Trainer(experiment_name='ppe_yolonas_run2',ckpt_root_dir=check_point)
+
+# download the dataset and annptations from roboflow
 rf = Roboflow(api_key="IuYv6KOKs5p62rFSLvGa")
 project = rf.workspace("objet-detect-yolov5").project("eep_detection-u9bbd")
 dataset = project.version(1).download("yolov5")
+
+dataset_params = {
+    'data_dir':'/content/EEP_Detection-1',
+    'train_images_dir':'train/images',
+    'train_labels_dir':'train/labels',
+    'val_images_dir':'valid/images',
+    'val_labels_dir':'valid/labels',
+    'test_images_dir':'test/images',
+    'test_labels_dir':'test/labels',
+    'classes': ['Protective_helmet', 'Shield', 'Jacket', 'Dust Mask', 'Eye wear', 'Glove', 'Protective boot']
+}
+
+train_data = coco_detection_yolo_format_train(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['train_images_dir'],
+        'labels_dir': dataset_params['train_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size':16,
+        'num_workers':2
+    }
+)
+
+val_data = coco_detection_yolo_format_val(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['val_images_dir'],
+        'labels_dir': dataset_params['val_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size':16,
+        'num_workers':2
+    }
+)
+
+test_data = coco_detection_yolo_format_val(
+    dataset_params={
+        'data_dir': dataset_params['data_dir'],
+        'images_dir': dataset_params['test_images_dir'],
+        'labels_dir': dataset_params['test_labels_dir'],
+        'classes': dataset_params['classes']
+    },
+    dataloader_params={
+        'batch_size':16,
+        'num_workers':2
+    }
+)
+
+clear_output()
+
+# now we instatntiatet the model here in this example we are using yolonas_s (ie small) which is the smallest and fastest
+model=models.get('yolo_nas_s',
+                 num_classes=len(dataset_params['classes']),
+                 pretrained_weights="coco"
+)
+
+
+# now we define the metrics and training parameters
+train_params = {
+    # ENABLING SILENT MODE
+    'silent_mode': True,
+    "average_best_models":True,
+    "warmup_mode": "linear_epoch_step",
+    "warmup_initial_lr": 1e-6,
+    "lr_warmup_epochs": 3,
+    "initial_lr": 5e-4,
+    "lr_mode": "cosine",
+    "cosine_final_lr_ratio": 0.1,
+    "optimizer": "Adam",
+    "optimizer_params": {"weight_decay": 0.0001},
+    "zero_weight_decay_on_bias_and_bn": True,
+    "ema": True,
+    "ema_params": {"decay": 0.9, "decay_type": "threshold"},
+    # ONLY TRAINING FOR 10 EPOCHS FOR THIS EXAMPLE NOTEBOOK
+    "max_epochs": 10,
+    "mixed_precision": True,
+    "loss": PPYoloELoss(
+        use_static_assigner=False,
+        # NOTE: num_classes needs to be defined here
+        num_classes=len(dataset_params['classes']),
+        reg_max=16
+    ),
+    "valid_metrics_list": [
+        DetectionMetrics_050(
+            score_thres=0.1,
+            top_k_predictions=300,
+            # NOTE: num_classes needs to be defined here
+            num_cls=len(dataset_params['classes']),
+            normalize_targets=True,
+            post_prediction_callback=PPYoloEPostPredictionCallback(
+                score_threshold=0.01,
+                nms_top_k=1000,
+                max_predictions=300,
+                nms_threshold=0.7
+            )
+        )
+    ],
+    "metric_to_watch": 'mAP@0.50'
+}
+
+# now we can train the model
+
+trainer.train(model=model,
+              training_params=train_params,
+              train_loader=train_data,
+              valid_loader=val_data)
+
+# get the best trained model
+best_model=models.get('yolo_nas_s',
+                      num_classes=len(dataset_params['classes']),
+                      checkpoint_path="checkpoints/ppe_yolonas_run2/ckpt_best.pth")
+
+# now we test this best trained on the test dataset
+trainer.test(model=best_model,
+            test_loader=test_data,
+            test_metrics_list=DetectionMetrics_050(score_thres=0.1, 
+                                                   top_k_predictions=300, 
+                                                   num_cls=len(dataset_params['classes']), 
+                                                   normalize_targets=True, 
+                                                   post_prediction_callback=PPYoloEPostPredictionCallback(score_threshold=0.01, 
+                                                                                                          nms_top_k=1000, 
+                                                                                                          max_predictions=300,                                                                              
+                                                                                                          nms_threshold=0.7)
+                                                  ))
+
+# running the predictions
+
+img_url = '/content/EEP_Detection-1/valid/images/20220721_161927_jpg.rf.56b8fd236d16af66703544bae6fb6d14.jpg'
+best_model.predict(img_url).show()
+
+input_video_path = f"/content/demonew.mp4"
+output_video_path = "detections.mp4"
+
+import torch
+device = 'cuda' if torch.cuda.is_available() else "cpu"
+
+best_model.to(device).predict(input_video_path).save(output_video_path)
